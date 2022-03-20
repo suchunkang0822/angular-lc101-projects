2f467d61e5709ca5188b9fe1ab3c285b70cdb62e
Su Kang <sukang2017@u.northwestern.edu>
2022-03-20 00:32:12 -0500

    Part1 Done

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Blah Blah Blah, Let\'s Go';
+  image1 = 'https://www.gannett-cdn.com/presto/2020/01/16/PNJM/1d076419-c075-4f59-a808-b2630d2b5d39-Bentley.jpg?crop=1919,1080,x0,y97&width=1600&height=800&format=pjpg&auto=webp';
+  image2 = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/porsche-cayenne-turbo-gt-6-jpg-1628785027.jpg?crop=0.775xw:0.516xh;0.00163xw,0.315xh&resize=1200:*';
+  image3 = 'https://di-uploads-pod15.dealerinspire.com/porscheoffarmingtonhills/uploads/2021/10/2022-Porsche-Cayenne-Turbo-GT.jpg';
 
   constructor() { }
 
