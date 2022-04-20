365871fddafabaedd5f5e7a9669f427cf14c9198
Su Kang <sukang2017@u.northwestern.edu>
2022-04-20 12:59:31 -0500

    exercises done

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { count } from 'console';
 
 @Component({
    selector: 'buttons',
@@ -7,7 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+   inactive_gold: boolean = false;
+   inactive_silver: boolean = false;
+   inactive_copper: boolean = false;
+
+   inactive_frustration: boolean = false;
+   counter: number = 0;
+
+    add():void{
+      if(this.counter >=2){
+        this.counter = 0
+      }
+      this.counter+=1
+      console.log(this.counter)
+    }
+
+    toggle(color:string):void{
+      if(color==="gold"){
+        this.inactive_gold = true;
+      }else if(color==="silver"){
+        this.inactive_silver = true;
+      }else if(color==="copper"){
+        this.inactive_copper=true;
+      }else{
+        this.inactive_gold = false;
+        this.inactive_silver = false;
+        this.inactive_copper = false;
+      }
+   }
+
+
+
+   lcLightBlue: string = 'green';
 
    constructor() { }
 
--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,34 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  handleTakeOff():void{
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+      this.color = 'blue';
+      this.height = 10000;
+      this.width = 0;
+      this.message = 'Shuttle in flight.';
+    }
+  }
+
+  handleLand(){
+    let result = window.confirm("The shuttle is landing. Landing gear engaged.")
+    if(result){
+      this.width = 0;
+      this.height = 0;
+      this.color = 'green';
+      this.message = "The shuttle has landed.";
+    }
+  }
+
+  handleAbortMission(){
+    let result = window.confirm("Do you really want to abort mission?");
+    if(result){
+      this.width = 0;
+      this.height = 0;
+      this.color = 'red';
+      this.message = "Mission aborted.";
+    }
+  }
 }
